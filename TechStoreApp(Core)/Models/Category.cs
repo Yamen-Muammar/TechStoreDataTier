9068b6c585017ece340d8d6f3e3ca41fad31afcd
Yamen Muammar <yamenmuamar@gmail.com>
2026-01-25 22:36:34 +0200

    Create Category Model

---
+++ TechStoreApp(Core)/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechStoreApp_Core_.Models
+{
+    public class Category
+    {
+        public int? Id { get; set; }
+        public string CategoryName { get; set; }
+
+        public Category(int? id , string categoryName)
+        {
+            Id = id;
+            CategoryName = categoryName;
+        }
+    }
+}
