7f4913419d516b69d578375fb77b0f751821512a
Yamen Muammar <yamenmuamar@gmail.com>
2026-01-25 22:40:38 +0200

    Create Permission Model

---
+++ TechStoreApp(Core)/Models/Permission.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechStoreApp_Core_.Models
+{
+    public class Permission
+    {
+        public int? Id { get; set; }
+        public string PermissionName { get; set; }
+        public int PermissionCode { get; set; }
+
+        public Permission(int ? id , string permissionName , int permissiomCode)
+        {
+            Id = id;
+            PermissionName = permissionName;
+            PermissionCode = permissiomCode;
+        }
+
+    }
+}
